ef46ba6f99b690b1a31c6bda565565c52306c05e
David-Fde <fernandezvazquezd@gmail.com>
2019-11-11 17:15:11 +0100

    Empezando con argpars

--- pruebas_main.py
+++ pruebas_main.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import tmdbsimple as tmdb
+tmdb.API_KEY = os.getenv("TOKEN")
+import pandas as pd
+import numpy as np
+import argparse
+
 def create_movies_df(movies):
     # It takes a list of movies as parameter and returns a pandas dataframe with columns:
     # title, vote_average, release_date, original_title, original_language, overview.
@@ -16,26 +25,15 @@ def create_movies_df(movies):
     return movies_df
 
 
+def parse():
+    parser = argparse.ArgumentParser()                 
+    
+    parser.add_argument('movies', help='Movies', type=list)
+    return parser.parse_args()
 
-def chronological_order(df):
-    return df.sort_values(by='release_date')
+def main(): 
+    args=parse()
+    print(create_movies_df(args.movies))
 
-def vote_average_desc(df):
-    return df.sort_values(by='vote_average',ascending=False)
-
-def max_min_vote_average(df,value):
-    if value == maximo:
-        return print(df[df['vote_average'] == df['vote_average'].max()])
-    elif value == minimo:
-        return print(df[df['vote_average'] == df['vote_average'].min()]) 
-
-def overview(df):
-    title=input('Tell me a movie and I tell you its overview: ')
-    #os.system('clear')
-    counter=1
-    for e in df['title']:
-        if e==title:
-            return print(df['overview'][counter])
-        else:
-            counter+=1
-
+if __name__=='__main__':
+    main()
